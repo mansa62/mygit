4e190d3bd706d5ac2ca76639282c1d1a958332fd
mansa62 <zeinab.akhbary@gmail.com>
2020-03-31 00:25:56 +0430

    First commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("hi");
+});
+
+app.listen(3000, () => {
+    console.log("server running PORT 3000...!");
+})
